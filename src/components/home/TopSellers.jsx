e90ac03022c5d1d4767080b358a2f160b73478f1
msui51 <maria.sui51@gmail.com>
2025-12-10 16:44:50 -0500

    added dynamic loading and routing

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import { Link, useParams } from "react-router-dom";
 
-const TopSellers = () => {
+
+const TopSellers = ({loading, topSellers}) => {
+  const {id} = useParams()
+  
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,7 +18,7 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {/* {new Array(12).fill(0).map((_, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
                     <Link to="/author">
@@ -30,6 +33,24 @@ const TopSellers = () => {
                   <div className="author_list_info">
                     <Link to="/author">Monica Lucas</Link>
                     <span>2.1 ETH</span>
+                  </div>
+                </li>
+              ))} */}
+              {topSellers.map((item)=> (
+                <li key={item.id}>
+                  <div className="author_list_pp">
+                    <Link to={`/author/${item.authorId}`}>
+                      <img
+                        className="lazy pp-author"
+                        src={item.authorImage}
+                        alt=""
+                      />
+                      <i className="fa fa-check"></i>
+                    </Link>
+                  </div>
+                  <div className="author_list_info">
+                    <Link to={`/author/${item.authorId}`}>{item.authorName}</Link>
+                    <span>{item.price} ETH</span>
                   </div>
                 </li>
               ))}
